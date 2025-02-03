12e1639bc2ced943f403676b1199b5dfb9114e82
Frida Lindbjerg <fridalindbjerg@outlook.dk>
2025-02-03 13:53:17 +0100

    :)

--- js/produktliste.js
+++ js/produktliste.js
@@ -3,12 +3,11 @@ fetch(`https://kea-alt-del.dk/t7/api/products/`)
   .then((response) => response.json())
   .then(showList);
 
-function showList(products) {
-  console.log(products);
-  let markup = "";
-  products
-    .map((product) => {
-      markup += `<article>
+function showList(data) {
+  const markup = data
+    .map(
+      (product) =>
+        `<article>
       <img src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp" alt="" />
       <h3>${product.productdisplayname}</h3>
       <h4>${product.articletype} &#124; ${product.brandname}</h4>
@@ -17,8 +16,8 @@ function showList(products) {
         <p>${product.discount}</p>
       </div>
       <a href="produkt.html">Read more</a>
-   </article>`;
-    })
+   </article>`
+    )
     .join("");
 
   console.log(markup);

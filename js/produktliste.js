13580ac67ee573ba09fefb7d1ce661f3cd9f9bef
Frida Lindbjerg <fridalindbjerg@outlook.dk>
2025-02-05 13:18:44 +0100

    final

--- js/produktliste.js
+++ js/produktliste.js
@@ -1,5 +1,3 @@
-// isOnSale is linje 14 fjerner produkter, der er udsolgt
-
 let listContainer = document.querySelector(".listcontainer");
 const mycategory = new URLSearchParams(window.location.search).get("category");
 
@@ -11,14 +9,14 @@ function showList(data) {
   const markup = data
     .map(
       (product) =>
-        `<article class=" ${product.soldout && "isOnSale"}"  >
-      <img src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp" alt="" />
+        `<article class=" ${product.soldout && "udsolgt"}"  >
+      <img src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp" alt="" /> 
       <h3>${product.productdisplayname}</h3>
       <h4>${product.articletype} &#124; ${product.brandname}</h4>
       <div>
         <h5 class="${product.discount && "overWrite"}">${product.price},-</h5>
         
-        ${product.discount > 0 ? `<span class="SaleLabel isOnSale">-${product.discount}%</span>` : ""}
+        ${product.discount > 0 ? `<span class="SaleLabel">-${product.discount}%</span>` : ""}
         ${product.discount > 0 ? `<h5>Now: ${product.price - product.discount},-</h5>` : ""}
         
         
